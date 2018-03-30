d305ceb7791673b7a0b79d7cc6211388a6b81734
James George <jamesgeorge998001@gmail.com>
2018-03-30 17:55:53 +0530

    Initial commit.

---
+++ JS/text-blink.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  Typed.new(".blink", {
+    strings: [
+        "Hey There People.",
+        "First of all Thanks for showing interest  on me.",
+        "Just some insights on myself.",
+      "Full Stack Web Developer.",
+      "Android Developer.",
+      "Youtuber.",
+      "Blogger.",
+      "Enthusiastic Learner."
+    ],
+    typeSpeed: 20,
+    loop: true,
+  });
+});
