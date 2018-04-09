e7dff5294afb0f89d7796691d191019eaa4e260f
James George <jamesgeorge998001@gmail.com>
2018-04-09 18:59:05 +0530

    Navbar improvements in mobile screens.

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById('btn')
+
+let navBar = document.getElementById('nav')
+
+let tracker = 0
+
+btn.addEventListener('click', () => {
+
+    tracker++
+    if (tracker % 2 == 0) {
+        navBar.className = 'nav-bar nav-expand'
+    } else {
+        navBar.className = 'nav-bar nav-contract'
+    }
+})
