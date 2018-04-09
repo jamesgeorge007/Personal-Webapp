f8171ef0f325fed32b912f2d60daaff6ca259fd9
James George <jamesgeorge998001@gmail.com>
2018-04-09 16:21:12 +0530

    Font changes as well as other UI improvements.

--- JS/text-blink.js
+++ JS/text-blink.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  Typed.new(".blink", {
-    strings: [
-        "Hey There People.",
-        "I'm James George.",
-        "First of all Thank You<br>for showing interest<br>in me.",
-        "Just some insights on myself.",
-      "Full Stack Web Developer.",
-      "Android Developer.",
-      "Youtuber.",
-      "Blogger.",
-      "Enthusiastic Learner."
-    ],
-    typeSpeed: -50,
-    backSpeed: -30,
-    loop: true,
-  });
+    Typed.new(".blink", {
+        strings: [
+            "Hey There People.",
+            "I'm James George.",
+            "Just some insights on myself.",
+            "Full Stack Web Developer.",
+            "Android Developer.",
+            "Youtuber.",
+            "Blogger.",
+            "Enthusiastic Learner."
+        ],
+        typeSpeed: -50,
+        backSpeed: -30,
+        loop: true,
+    });
 });
