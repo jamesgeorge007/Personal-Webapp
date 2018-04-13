0b52ef93ad3e3ec3db7b2a4e3b97a5773872b71b
James George <jamesgeorge998001@gmail.com>
2018-04-13 21:51:23 +0530

    add articles section.

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ let tracker = 0
 btn.addEventListener('click', () => {
 
     tracker++
-    if (tracker % 2 == 0) {
+    if (tracker % 2 != 0) {
         navBar.className = 'nav-bar nav-expand'
     } else {
         navBar.className = 'nav-bar nav-collapse'
