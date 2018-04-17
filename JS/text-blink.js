ffad1f0501e563e6d01423087c4b639a4aa5ecb0
James George <jamesgeorge998001@gmail.com>
2018-04-17 15:10:51 +0530

    Add certifications section.

--- JS/text-blink.js
+++ JS/text-blink.js
@@ -10,8 +10,26 @@ document.addEventListener("DOMContentLoaded", function() {
             "Blogger.",
             "Enthusiastic Learner."
         ],
-        typeSpeed: -50,
-        backSpeed: -30,
+        typeSpeed: -180,
+        backSpeed: -170,
+        backDelay: 0,
         loop: true,
     });
+
+     Typed.new(".typed", {
+       strings: [
+         "Hey There People.",
+         "I'm James George.",
+         "Just some insights on myself.",
+         "Full Stack Developer.",
+         "Android Developer.",
+         "Youtuber.",
+         "Blogger.",
+         "Enthusiastic Learner."
+       ],
+       typeSpeed: 70,
+       backSpeed: 50,
+       backDelay: 500,
+       loop: true
+     });
 });
