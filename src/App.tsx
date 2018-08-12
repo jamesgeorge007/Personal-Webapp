261a1dad6d43ba3dd3b0c3f38a386fdc1658419f
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-12 13:49:39 +0530

    Stick basck to the bootstrap ways :+1:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
+      <NavBar />
         <h1>James George</h1>
       </div>
     );
