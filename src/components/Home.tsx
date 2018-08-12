5f15a0fe4262d2fa75070871fde8edde2941a058
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-12 14:52:49 +0530

    Initial structure :smiley:

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './assets/css/Home.css';
+
+class Home extends React.Component<{}, any>{
+    public render() {
+        return (
+            <div className='home-wrapper'>
+             <h1>James George</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
+import { Nav, Navbar, NavItem  } from 'react-bootstrap';
 
 class NavBar extends React.Component<{}, any>{
       public render() {
         return (
           <div>
-            
+            <Navbar style={{borderRadius: 0, backgroundColor: '#000'}} inverse={true} collapseOnSelect={true}>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#brand">JG</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav pullRight={true}>
+      <NavItem eventKey={1} href="#">
+        Portfolio
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Get In Touch
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
           </div>
         );
       }
