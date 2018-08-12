616a9c382a6876326cdadf5e2103727118a80762
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-12 15:19:35 +0530

    Workarounds :smiley:

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,9 +4,16 @@ import './assets/css/Home.css';
 class Home extends React.Component<{}, any>{
     public render() {
         return (
-            <div className='home-wrapper'>
-             <h1>James George</h1>
-            </div>
+            <div>
+                <div className="bg" />
+                <div className='home-wrapper'>
+                <div className="image-wrapper">
+                    <img className="profile-img" src="https://avatars2.githubusercontent.com/u/25279263?s=400&v=4" alt="profile-image"/>
+                </div>
+                    <h1>James George</h1>
+                    <p className="lead">Yet another Developer</p>
+                </div>
+                </div>
         );
     }
 }
