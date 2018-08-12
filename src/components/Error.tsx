fe731829eb5aa1b869b7fdfa2ea47c5c0a06d717
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-12 21:12:03 +0530

    Routing bug fixes :+1:

---
+++ src/components/Error.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Error extends React.Component{
+    public render(){
+            return (
+                <div>
+                    <h2>This route doesn't exist!</h2>
+                </div>
+            );
+    }
+}
+
+export default Error;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,44 @@
 import * as React from 'react';
 import { Nav, Navbar, NavItem  } from 'react-bootstrap';
+import { BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+import Contact from './Contact';
+import Error from './Error';
+import Home from './Home';
+import Portfolio from './Portfolio';
+
 
 class NavBar extends React.Component<{}, any>{
       public render() {
         return (
           <div>
+            <BrowserRouter>
+            <div>
             <Navbar style={{borderRadius: 0, backgroundColor: '#000', marginBottom: -.5}} inverse={true} collapseOnSelect={true}>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#brand" style={{color: '#fff', fontFamily: 'forte'}}>JG</a>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav pullRight={true}>
-      <NavItem eventKey={1} href="#">
-        Portfolio
-      </NavItem>
-      <NavItem eventKey={2} href="#">
-        Get In Touch
-      </NavItem>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+      <Link to="/" style={{outline: 'none'}}><a style={{color: '#fff', fontFamily: 'forte', textDecoration: 'none'}}>JG</a></Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight={true}>
+        <NavItem eventKey={1}>
+          <Link to="/portfolio" style={{textDecoration: 'none', color: '#fff', outline: 'none'}}>Portfolio</Link>
+        </NavItem>
+        <NavItem eventKey={2}>
+          <Link to="/contact" style={{textDecoration: 'none', color: '#fff', outline: 'none'}}>Get In Touch</Link>
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/portfolio' component={Portfolio}/>
+          <Route path='/contact' component={Contact}/>
+          <Route path='*' component={Error}/>
+    </Switch>
+    </div>      
+  </BrowserRouter>
           </div>
         );
       }
