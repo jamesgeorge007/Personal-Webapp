021ee489e00edf7e0558704608d186ab8c14e287
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-12 13:49:39 +0530

    Stick basck to the bootstrap ways :+1:

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class NavBar extends React.Component<{}, any>{
+      public render() {
+        return (
+          <div>
+            
+          </div>
+        );
+      }
+}
+
+export default NavBar;
