031e48bcd8046ec7d2ef0a3bd41214d753bf50a4
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-17 22:21:29 +0530

    Random workarounds :+1:

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -8,19 +8,19 @@ class Portfolio extends React.Component{
                     <div className="row card-wrapper">
                         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <div className="card">
-                                <img className="portfolio-img" src="https://image.flaticon.com/icons/png/512/176/176684.png" alt="work"/>
+                                <img className="portfolio-img" src="https://png.icons8.com/small/1600/browser-window.png" alt="web"/>
                             </div>    
                         </div>
 
                         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <div className="card">
-                                <img className="portfolio-img" src="https://img.clipartxtras.com/11dd8ebdbaeeb7bcbc5bf0e8b104d6ff_android-phone-clipart-clipartxtras-android-mobile-clipart_512-512.png" alt="work"/>
+                                <img className="portfolio-img" src="https://cdn4.iconfinder.com/data/icons/miu/24/device-iPhone-smartphone-vertical-outline-stroke-512.png" alt="mobile"/>
                             </div>    
                         </div>
 
                         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <div className="card">
-                                <img className="portfolio-img" src="http://www.freebiesgallery.com/wp-content/uploads/2014/04/imac.png" alt="work"/>
+                                <img className="portfolio-img" src="https://www.shareicon.net/download/2015/08/26/91130_monitor.ico" alt="pc"/>
                             </div>    
                         </div>
                     </div>
