47a187138ab51cbe24465ccd6b7c5078b9c07118
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-29 17:10:46 +0530

    Change Navbar as part of migration from Bootstrap 3 to 4 :rocket:

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -5,45 +5,14 @@ class Portfolio extends React.Component{
     public render(){
             return (
                 <div>
-                    <div className="jumbotron">
-                        <p className="lead">
-                        I build solutions for the web, mobile and pc.
-                        </p>
-                    </div>
-                    <div className="row card-wrapper">
-                        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                            <div className="card">
-                                <img className="portfolio-img" src="https://png.icons8.com/small/1600/browser-window.png" alt="web"/>
-                                <div className="desc">
-                                    <p className="lead">
-                                        Web apps built with popular technology stacks like LAMP, MEVN and MERN folllowing responsive guidelines.
-                                    </p>
-                                </div>
-                            </div>    
-                        </div>
-
-                        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                            <div className="card">
-                                <img className="portfolio-img" src="https://cdn4.iconfinder.com/data/icons/miu/24/device-iPhone-smartphone-vertical-outline-stroke-512.png" alt="mobile"/>
-                                <div className="desc">
-                                    <p className="lead">
-                                        Native mobile apps built the normal Java, kotlin way or even using cross platform frameworks like React Native.
-                                    </p>
-                                </div>
-                            </div>    
-                        </div>
-
-                        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                            <div className="card">
-                                <img className="portfolio-img" src="https://www.shareicon.net/download/2015/08/26/91130_monitor.ico" alt="pc"/>
-                                <div className="desc">
-                                    <p className="lead">
-                                        Cross platform Desktop apps built on top of Electron adopting modern design principles.
-                                    </p>
-                                </div>
-                            </div>    
-                        </div>
-                    </div>
+                    <div className="card">
+  <img className="card-img-top" src="img_avatar1.png" alt="Card image" />
+  <div className="card-body">
+    <h4 className="card-title">John Doe</h4>
+    <p className="card-text">Some example text.</p>
+    <a href="#" className="btn btn-primary">See Profile</a>
+  </div>
+</div>
                 </div>
             );
     }
