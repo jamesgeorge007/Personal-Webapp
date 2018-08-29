4e35717ed185e965f29e1b17a29be0f873b4f638
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-29 19:58:36 +0530

    Portfolio section workarounds :+1:

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -4,16 +4,41 @@ import './assets/css/Portfolio.css';
 class Portfolio extends React.Component{
     public render(){
             return (
-                <div>
-                    <div className="card">
-  <img className="card-img-top" src="img_avatar1.png" alt="Card image" />
-  <div className="card-body">
-    <h4 className="card-title">John Doe</h4>
-    <p className="card-text">Some example text.</p>
-    <a href="#" className="btn btn-primary">See Profile</a>
-  </div>
-</div>
+            <div>
+                <div className="jumbotron">
+                    <p className="lead">
+                        I build cross platform solutions for the web, mobile and pc keeping up with the latest industry standards.
+                    </p>
                 </div>
+                <div className="card-wrapper">
+                    <div className="card" style={{width: '22em', textAlign: 'center'}}>
+                        <i className="card-img-top fas fa-globe fa-5x" />
+                        <div className="card-body">
+                            <h5 className="card-title">Web</h5>
+                            <p className="card-text">Web apps built with popular technology stacks like LAMP, MEVN and MERN folllowing responsive guidelines.</p>
+                            <a href="https://www.github.com/jamesgeorge007" target="_blank" className="btn">Explore</a>
+                        </div>
+                    </div>
+
+                    <div className="card" style={{width: '22em', textAlign: 'center'}}>
+                    <i className="card-img-top fas fa-mobile-alt fa-5x" />
+                        <div className="card-body">
+                            <h5 className="card-title">Mobile</h5>
+                            <p className="card-text">Native mobile apps built the normal Java, kotlin way as well as using cross platform frameworks like React Native.</p>
+                            <a href="https://www.github.com/jamesgeorge007" target="_blank" className="btn">Explore</a>
+                        </div>
+                    </div>
+
+                    <div className="card" style={{width: '22em', textAlign: 'center'}}>
+                    <i className="card-img-top fas fa-desktop fa-5x" />
+                    <div className="card-body">
+                            <h5 className="card-title">PC</h5>
+                            <p className="card-text">Cross platform Desktop apps built on top of Electron adopting modern design principles.</p>
+                            <a href="https://www.github.com/jamesgeorge007" target="_blank" className="btn">Explore</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
             );
     }
 }
