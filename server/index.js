a86b13cd8ea77f546ed016c2c23bf9fb35a9b802
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-31 16:51:14 +0530

    Establish link between client and server parts :rocket:

--- server/index.js
+++ server/index.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.urlencoded({
     extended: true
   }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/api', require('./routes/api'))
+app.use('/api', require('./routes/api'));
 
+app.use(cors());
 
-app.use(cors())
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 
 mongoose.connect('mongodb://randomguy:randomguy123@ds018508.mlab.com:18508/get-in-touch-records')
   .catch((err) => {
     console.error('eror: ' + err.stack)
     process.exit(1)
-  })
+  });
 mongoose.connection.on('open', () => {
   console.log('connected to database')
-})
+});
 mongoose.Promise = global.Promise
 
 app.listen(process.env.port || 4000, () => {
