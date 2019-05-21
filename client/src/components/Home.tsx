81fb9300427e2aebeeaa4eeb2d3def0c7ada9ca5
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-05-21 18:53:47 +0530

    Tweaks

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,6 +3,12 @@ import '../assets/css/Home.css';
 
 class Home extends React.Component<{}, any>{
     public render() {
+        const profileIcons = {
+            'github': 'https://github.com/jamesgeorge007',
+            'linkedin-in': 'https://linekdin.com/in/jamesgeorge007',
+            'twitter': 'https://twitter.com/james_madhacks'
+        };
+        const colors = ['#fff', 'darkblue', 'lightblue'];
         return (
             <div>
                 <div className="bg" />
@@ -13,9 +19,9 @@ class Home extends React.Component<{}, any>{
                     <h1>James George</h1>
                     <p className="lead" style={{fontWeight: 400}}>Yet another Developer</p>
                     <div className="social-profile-icons">
-                        <i className="fab fa-twitter icon" />
-                        <i className="fab fa-github icon" />
-                        <i className="fab fa-linkedin-in icon" />
+                    { Object.keys(profileIcons).map((item, key) => {
+                            return <a key={key} href={profileIcons[item]} target="__blank"><i className={`fab fa-${item} icon`} style={{color: colors[key]}}/></a>
+                        })}
                     </div>
                 </div>
                 </div>
