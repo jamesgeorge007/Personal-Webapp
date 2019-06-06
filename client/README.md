515738e93dd3b16535bec8c67c78ddfcaef47e04
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-06-06 10:13:56 +0530

    Change in project hierarchy

--- client/README.md
+++
@@ -1,3 +0,0 @@
-## Personal Website
-
-> Under :construction:
--- server/routes/api.js
+++
@@ -1,6 +0,0 @@
-import saveResponse   from '../controllers/contact_controller';
-import express from 'express';
-const router = express.Router();
-router.post('/contact', saveResponse);
-
- module.exports = router;
